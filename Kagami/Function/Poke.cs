078d0cbb02a78a8dfec96825dd06874893d13d48
Poker <poker_sang@outlook.com>
2022-04-24 21:53:21 +0800

    new language feature

--- Kagami/Function/Poke.cs
+++ Kagami/Function/Poke.cs
@@ -2,21 +2,20 @@
 using Konata.Core.Events.Model;
 using Konata.Core.Interfaces.Api;
 
-namespace Kagami.Function
-{
-    public static class Poke
-    {
-        /// <summary>
-        /// On group poke
-        /// </summary>
-        /// <param name="bot"></param>
-        /// <param name="group"></param>
-        internal static void OnGroupPoke(Bot bot, GroupPokeEvent group)
-        {
-            if (group.MemberUin != bot.Uin) return;
+namespace Kagami.Function;
 
-            // Convert it to ping
-            bot.SendGroupMessage(group.GroupUin, Command.OnCommandPing(null));
-        }
+public static class Poke
+{
+    /// <summary>
+    /// On group poke
+    /// </summary>
+    /// <param name="bot"></param>
+    /// <param name="group"></param>
+    internal static void OnGroupPoke(Bot bot, GroupPokeEvent group)
+    {
+        if (group.MemberUin != bot.Uin) return;
+
+        // Convert it to ping
+        bot.SendGroupMessage(group.GroupUin, Command.OnCommandPing(null));
     }
 }
